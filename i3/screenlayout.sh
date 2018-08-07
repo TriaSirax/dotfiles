af1ae27b9c8a39432b9f61b6980ec97e955dfcb7
TriaSirax <40128336+TriaSirax@users.noreply.github.com>
2018-08-07 17:21:59 +0000

    Add files via upload

---
+++ i3/screenlayout.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if glxinfo | grep "server glx vendor string: NVIDIA"; then
+    intern=eDP-1-1
+    extern=HDMI-1-2
+    if xrandr | grep "$extern disconnected"; then
+        xrandr --output "$extern" --off --output "$intern" --auto
+    else
+        xrandr --output "$intern" --off --output "$extern" --mode ultrawide60 
+    fi
+else
+    intern=eDP1
+    extern=HDMI2
+    if xrandr | grep "$extern disconnected"; then
+        xrandr --output "$extern" --off --output "$intern" --auto
+    else
+        xrandr --output "$intern" --off --output "$extern" --mode ultrawide60 
+    fi
+fi
